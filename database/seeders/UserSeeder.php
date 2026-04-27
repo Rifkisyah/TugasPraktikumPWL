05b8e7b1034f523176b865f0ef2e26cb013b764e
Hashirunn <karunyarizqi@gmail.com>
2026-04-27 09:01:00 +0700

    Authentication and Authorization

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,10 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class UserSeeder extends Seeder
 {
@@ -14,13 +16,25 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('users')->insert([
-            'npm' => 5520123024,
-            'username' => 'User 1',
-            'first_name' => 'User',
+        Role::create(['name' => 'pustakawan']);
+        Role::create(['name' => 'mahasiswa']);
+
+        User::create([
+            'npm' => 001,
+            'username' => 'Pustakawan',
+            'first_name' => 'Pustakawan',
             'last_name' => '1',
-            'email' => 'user1@gmail.com',
+            'email' => 'pustakawan@gmail.com',
             'password' => Hash::make('password123'),
-        ]);
+        ])->assignRole('pustakawan');
+
+        User::create([
+            'npm' => 5520123024,
+            'username' => 'Mahasiswa',
+            'first_name' => 'Mahasiswa',
+            'last_name' => '1',
+            'email' => 'mhs1@gmail.com',
+            'password' => Hash::make('password123'),
+        ])->assignRole('mahasiswa');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,36 +1,20 @@
 <?php
 
-use App\Models\Book;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// route return string
-Route::get('/test', function(){
-    return 'Hello World';
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/books', function(){
-    $data['books'] = Book::all();
-    return view('books.index', $data);
-});
-
-// route view
-// Route::view('/books', 'books.index');
-
-// route view with data
-Route::view('/greeting', 'greeting', ['name' => 'John Doe']);
-
-// route with parameter
-Route::get('/greeting/{name}', function($name){
-    return view('greeting', ['name' => $name]);
-});
-
-// route dengan controller
-Route::controller(UserController::class)->group(function () {
-    // Route::get('/greeting', 'index'); // tidak pakai parameter
-    Route::get('/greeting/{name}', 'index');
-});
+require __DIR__.'/auth.php';
